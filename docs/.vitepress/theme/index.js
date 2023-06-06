ab072467eb2385d65df08cdfb63a62ddd6d5435e
mlengse <medtosys@gmail.com>
2023-06-06 12:49:25 +0700

    asda

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import Kaling from './layout/kaling.vue'
-// import './style.css'
+import './style.css'
 // import type { App } from 'vue'
 import { anu } from 'anu-vue'
 import 'uno.css'
-import 'anu-vue/dist/style.css'
-import '@anu-vue/preset-theme-default/dist/style.css'
+// import 'anu-vue/dist/style.css'
+// import '@anu-vue/preset-theme-default/dist/style.css'
 
 export default {
   ...DefaultTheme,
